0a897d82396c445e51479377e9c1ce6c7b348cac
Egor Andreevici <andreevich.egor@gmail.com>
2015-07-05 16:41:30 +0200

    Fixing Travis build #10

--- app/src/androidTest/java/me/egorand/dagger_2_testing/BaseDataLoadingTest.java
+++ app/src/androidTest/java/me/egorand/dagger_2_testing/BaseDataLoadingTest.java
@@ -33,7 +33,9 @@ import rx.Observable;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.contrib.RecyclerViewActions.scrollToPosition;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.mock;
@@ -104,6 +106,7 @@ public abstract class BaseDataLoadingTest {
 
         for (int position = 0; position < TestData.REPOS.size(); position++) {
             Repo repo = TestData.REPOS.get(position);
+            onView(withId(android.R.id.list)).perform(scrollToPosition(position));
             onView(withText(repo.name)).check(matches(isDisplayed()));
             onView(withText(repo.description)).check(matches(isDisplayed()));
         }
